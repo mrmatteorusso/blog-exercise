bbf3634fefc7ba1d4cc17cdb01baa08f85713b1e
mrmatteorusso <mrmatteorusso@gmail.com>
2022-10-03 11:23:40 +0200

    mysqli connection

--- code/public/index.php
+++ code/public/index.php
@@ -1,3 +1,13 @@
 <?php
 
-phpinfo();
+//phpinfo();
+echo "ciao";
+
+$connection = mysqli_connect(hostname: 'localhost:8902', username: 'root', password: 'password', database: 'EXERCISE01');
+echo $connection;
+exit;
+echo "ciao";
+$query = "SELECT * FROM blog";
+$mysqli_query = mysqli_query($connection, $query);
+
+echo $mysqli_query;
