10c62c8be40420108ee2feb2bdff20ff4515f3ec
mrmatteorusso <mrmatteorusso@gmail.com>
2022-10-03 18:35:31 +0200

    bootstrap design

--- code/public/index.php
+++ code/public/index.php
@@ -37,10 +37,6 @@ try {
 
 
 $data = $pdo->query('SELECT * FROM blog')->fetchAll(PDO::FETCH_ASSOC);
-// $data->execute();
-echo "<pre>";
-print_r($data);
-echo "</pre>";
 ?>
 
 <!DOCTYPE html>
@@ -50,31 +46,87 @@ echo "</pre>";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <title>Super Blog</title>
 </head>
 
 <body>
+    <h1 class="mb-5">Super Blog</h1>
 
-    <?php foreach ($data as $product => $attribute) {
+    <form method="POST" class="mb-5">
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" id="title" placeholder="name@example.com">
+        </div>
+        <div class="form-group">
+            <label for="category">Category</label>
+            <select class="form-control" id="category">
+                <option>Choose</option>
+                <option>Music</option>
+                <option>Book</option>
+                <option>Cinema</option>
+                <option>Teathre</option>
+                <option>Other..</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="content">Content</label>
+            <textarea class="form-control" id="content" rows="3"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="user_name">User Name</label>
+            <input type="text" class="form-control" id="user_name">
+        </div>
+        <button type="button" class="btn btn-primary px-3">Save</button>
+    </form>
 
-    ?>
 
-        <h1><?php echo $data[$product]['title']; ?></h1>
-        <p><?php echo $data[$product]['content']; ?></p>
-        <p><?php echo $data[$product]['user_name']; ?></p>
-        <p><?php echo $data[$product]['created_at']; ?></p>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Content</th>
+                <th scope="col">Category</th>
+                <th scope="col">User</th>
+                <th scope="col">Created on</th>
+                <th scope="col">Updated</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+            </tr>
+        </thead>
 
-    <?php
-    }
+        <tbody>
+            <?php foreach ($data as $product => $attribute) {
 
-    ?>
+            ?>
+                <tr>
+                    <th scope="row"><?php echo $data[$product]['id']; ?></th>
+                    <td><?php echo $data[$product]['title']; ?></td>
+                    <td><?php echo $data[$product]['content']; ?></td>
+                    <td><?php echo $data[$product]['category_name']; ?></td>
+                    <td><?php echo $data[$product]['user_name']; ?></td>
+                    <td><?php echo $data[$product]['created_at']; ?></td>
+                    <td><?php echo empty($data[$product]['updated']) ? "-" : $data[$product]['updated']; ?></td>
+                    <td><button type="button" class="btn btn-primary px-3">Edit</button></td>
+                    <td><button type="button" class="btn btn-danger px-3">Delete</button></td>
+                </tr>
+
+            <?php
+            }
+
+            ?>
+        </tbody>
+
+    </table>
 
 </body>
 
 </html>
 
 
-
+<?php
 //my_sqli
 // $host = 'mysqlblog';
 // $db = 'test';
